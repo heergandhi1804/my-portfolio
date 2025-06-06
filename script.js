637cf74a04a1330104491bfb9537a4d5d5464a63
Heer Gandhi <heergandhi1804@gmail.com>
2025-06-06 17:37:39 +0530

    Initial upload of Website 2.0

---
+++ script.js
@@ -0,0 +1,8 @@
+const intro = document.getElementById("intro");
+const nav = document.getElementById("nav");
+const contact = document.getElementById("contact");
+const projects = document.getElementById("projects");
+const main = document.getElementById("main");
+const certi = document.getElementById("certifications");
+const expi = document.getElementById("experience");
+
